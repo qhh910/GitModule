754f73154ff9fa02a5051524f46f0176ec09ff9a
qhh <qhh7@123456.com>
2022-01-10 15:28:56 +0800

    第3次提交

---
+++ src/main/java/com/qhh/gittest/HelloWord.java
@@ -0,0 +1,10 @@
+package com.qhh.gittest;
+//  com.qhh.gittest-HelloWord.java    @author: QHH  
+//  创建时间：2022-01-10-15:04
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+        System.out.println("123456");
+    }
+}
