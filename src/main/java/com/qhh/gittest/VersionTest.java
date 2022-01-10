86a8d88aebd6ac5a07fd9140f17eb076e5b0c29d
qhh <qhh7@123456.com>
2022-01-10 16:10:12 +0800

    版本切换：version3

--- src/main/java/com/qhh/gittest/VersionTest.java
+++ src/main/java/com/qhh/gittest/VersionTest.java
@@ -6,5 +6,6 @@ public class VersionTest {
     public static void main(String[] args) {
         System.out.println("version...........1");
         System.out.println("version...........2");
+        System.out.println("version...........3");
     }
 }
