e9d2e3c20e26dea6ec5b310b25124424a4739737
qhh <qhh7@123456.com>
2022-01-10 16:50:54 +0800

    合并分支：hot-fix分支上的修改

--- src/main/java/com/qhh/gittest/VersionTest.java
+++ src/main/java/com/qhh/gittest/VersionTest.java
@@ -8,5 +8,6 @@ public class VersionTest {
         System.out.println("version...........2");
         System.out.println("version...........3");
         System.out.println("version---在hot-fix分支上进行修改");
+        System.out.println("hot-fix  test");
     }
 }
