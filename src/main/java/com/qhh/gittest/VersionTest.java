3b3e69650ea877a6d22ddbf289711b2c45f12c85
qhh <qhh7@123456.com>
2022-01-10 16:09:12 +0800

    版本切换：version1

---
+++ src/main/java/com/qhh/gittest/VersionTest.java
@@ -0,0 +1,9 @@
+package com.qhh.gittest;
+//  com.qhh.gittest-VersionTest.java    @author: QHH  
+//  创建时间：2022-01-10-16:07
+
+public class VersionTest {
+    public static void main(String[] args) {
+        System.out.println("version...........1");
+    }
+}
